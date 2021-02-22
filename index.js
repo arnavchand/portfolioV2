d15df9b69ced432e62096ad05cdbf16b06605c8f
arnavchand <43096316+arnavchand@users.noreply.github.com>
2021-02-23 01:58:15 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-
-
 function myFunction() {
   navigator.clipboard.writeText("arnavchand@gmail.com")
 }
+
+document.querySelector(".get-in-touch-button").addEventListener("click", function() {
+  document.querySelector(".get-in-touch-button").innerHTML="Email Copied ✔";
+  document.querySelector(".get-in-touch-button").style.color="#000000";
+  document.querySelector(".get-in-touch-button").style.fontWeight="700";
+  document.querySelector(".get-in-touch-button").style.backgroundColor="#FAFAFA";
+  document.querySelector(".get-in-touch-button").style.borderColor= "transparent";
+});
